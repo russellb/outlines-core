1792544753ba7d4243df715fca6298219b300dbe
Rémi Louf <remi@thetypicalset.com>
2024-08-15 13:50:59 +0200

    Remove OpenAI integration and vectorization utils

--- outlines/generate/choice.py
+++ outlines/generate/choice.py
@@ -2,7 +2,6 @@ from functools import singledispatch
 from typing import Callable, List
 
 from outlines.generate.api import SequenceGeneratorAdapter
-from outlines.models import OpenAI
 from outlines.samplers import Sampler, multinomial
 
 from .regex import regex
@@ -18,19 +17,3 @@ def choice(
     generator.format_sequence = lambda x: x
 
     return generator
-
-
-@choice.register(OpenAI)
-def choice_openai(
-    model: OpenAI, choices: List[str], sampler: Sampler = multinomial()
-) -> Callable:
-    if not isinstance(sampler, multinomial):
-        raise NotImplementedError(
-            r"The OpenAI API does not support any other sampling algorithm "
-            + "that the multinomial sampler."
-        )
-
-    def generate_choice(prompt: str, max_tokens: int = 1):
-        return model.generate_choice(prompt, choices, max_tokens)
-
-    return generate_choice
--- outlines/models/__init__.py
+++ outlines/models/__init__.py
@@ -11,7 +11,6 @@ from typing import Union
 from .exllamav2 import ExLlamaV2Model, exl2
 from .llamacpp import LlamaCpp, llamacpp
 from .mlxlm import MLXLM, mlxlm
-from .openai import OpenAI, azure_openai, openai
 from .transformers import Transformers, TransformerTokenizer, mamba, transformers
 from .transformers_vision import TransformersVision, transformers_vision
 from .vllm import VLLM, vllm
