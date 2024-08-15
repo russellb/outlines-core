fc06746450ec1ffd9bea7486effa2c5e3d66cc5e
Rémi Louf <remi@thetypicalset.com>
2024-08-15 13:47:56 +0200

    Remove prompts and functions

--- outlines/__init__.py
+++ outlines/__init__.py
@@ -5,14 +5,10 @@ import outlines.processors
 import outlines.types
 from outlines.base import vectorize
 from outlines.caching import clear_cache, disable_cache, get_cache
-from outlines.function import Function
-from outlines.prompts import prompt
 
 __all__ = [
     "clear_cache",
     "disable_cache",
     "get_cache",
-    "Function",
-    "prompt",
     "vectorize",
 ]
