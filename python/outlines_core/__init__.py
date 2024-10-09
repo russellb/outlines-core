b755e4d72a85e379d43e060d5a0a0530b5a5c114
Andrew Lapp <andrew@github.rew.la>
2024-10-08 21:16:13 -0400

    Retrieve the version number at runtime

--- python/outlines_core/__init__.py
+++ python/outlines_core/__init__.py
@@ -1,4 +1,12 @@
 """Outlines is a Generative Model Programming Framework."""
+from importlib.metadata import PackageNotFoundError, version
+
 import outlines_core.models
+
+try:
+    __version__ = version("outlines_core")
+except PackageNotFoundError:
+    pass
+
 
 __all__ = ["models"]
