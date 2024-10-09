4c6c3769a0c703b08ad6da1edc2a1298c12b5e25
Rémi Louf <remi@thetypicalset.com>
2024-10-09 21:48:09 +0200

    Remove transformation from function to JSON Schema

--- python/outlines_core/fsm/json_schema.py
+++ python/outlines_core/fsm/json_schema.py
@@ -1,9 +1,3 @@
-import inspect
-import warnings
-from typing import Callable
-
-from pydantic import create_model
-
 from .outlines_core_rs import (  # noqa: F401
     BOOLEAN,
     DATE,
@@ -19,31 +13,3 @@ from .outlines_core_rs import (  # noqa: F401
     build_regex_from_schema,
     to_regex,
 )
-
-
-def get_schema_from_signature(fn: Callable) -> str:
-    """Turn a function signature into a JSON schema.
-
-    Every JSON object valid to the output JSON Schema can be passed
-    to `fn` using the ** unpacking syntax.
-
-    """
-    signature = inspect.signature(fn)
-    arguments = {}
-    for name, arg in signature.parameters.items():
-        if arg.annotation == inspect._empty:
-            raise ValueError("Each argument must have a type annotation")
-        else:
-            arguments[name] = (arg.annotation, ...)
-
-    try:
-        fn_name = fn.__name__
-    except Exception as e:
-        fn_name = "Arguments"
-        warnings.warn(
-            f"The function name could not be determined. Using default name 'Arguments' instead. For debugging, here is exact error:\n{e}",
-            category=UserWarning,
-        )
-    model = create_model(fn_name, **arguments)  # type: ignore
-
-    return model.model_json_schema()
